97a29197f3122f699589bca2dea89a88dbae0170
Demonyan <32725399+Demonya@users.noreply.github.com>
2021-08-18 20:03:41 +0800

    Update P3.3：MNIST数据集.py

--- P3/P3.3：MNIST数据集.py
+++ P3/P3.3：MNIST数据集.py
@@ -4,11 +4,12 @@
 #   导入MNIST数据集：mnist = tf.keras.datasets.mnist
 #   （x_train,y_train）,(x_test,y_test) = mnist.load_data()
 #   作为输入特征,输入神经网络时，将数据拉伸为一维数组：
-#   tf.keras.layers.Flatten() 
+#   tf.keras.layers.Flatten()
 
 import tensorflow as tf
 import matplotlib.pyplot as plt
 
 mnist = tf.keras.datasets.mnist
 (x_train, y_train),  (x_test, y_test) = mnist.load_data()
-plt.imshow(x_train[0])
+plt.imshow(x_train[1])
+plt.show()
